6b6397b007f95f394a4ccf0edf09f38fb0caeac4
JORGE <jorsalda@gmail.com>
2026-01-22 17:15:14 -0500

    Base de datos temporal

--- config.py
+++ config.py
@@ -1,31 +1,11 @@
-# config.py
 import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
-load_dotenv()
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave_super_segura'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/permisos'# ← SIN CONTRASEÑA, SIN DOS PUNTOS
+
+    # Base de datos temporal (SQLite) SOLO para que Render arranque
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///temp.db'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
-
-def test_db_connection():
-    """Prueba si la conexión a la base de datos es exitosa."""
-    print("🔍 Verificando conexión a la base de datos...")
-
-    try:
-        engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-        with engine.connect() as conn:
-           print("✅ Conexión exitosa a MySQL (desde config.py)")
-    except OperationalError as e:
-        print("❌ Error de conexión a MySQL:")
-        print(e)
-    except Exception as ex:
-        print("⚠️ Error inesperado al conectar:")
-        print(ex)
-
-# Ejecutar la prueba solo si este archivo se ejecuta directamente
-if __name__ == "__main__":
-    test_db_connection()
