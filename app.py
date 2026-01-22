f7d08bc37eadbeced222f4342e84311afd4a2c47
JORGE <jorsalda@gmail.com>
2026-01-22 16:52:41 -0500

    actiava cambion appr

--- app.py
+++ app.py
@@ -310,5 +310,10 @@ with app.app_context():
     db.create_all()
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
